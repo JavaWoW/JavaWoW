e922e0b8fe82e0fbcaf4a69e660bd710945ab27f
Jon Huang <jon5477@users.noreply.github.com>
2015-03-26 23:58:25 -0600

    Added debug code.

--- src/realm/handler/RealmVerifyHandler.java
+++ src/realm/handler/RealmVerifyHandler.java
@@ -1,5 +1,7 @@
 package realm.handler;
 
+import java.util.Arrays;
+
 import org.apache.mina.core.session.IoSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,13 +27,27 @@ public final class RealmVerifyHandler implements BasicHandler {
 
 	@Override
 	public final void handlePacket(IoSession session, SeekableLittleEndianAccessor slea) {
-		slea.readByte(); // ?
-		slea.readByte(); // ?
-		short buildNumber = slea.readShort(); // build number of the client
-		slea.skip(6); // ?
+		slea.skip(2); // ?
+		int buildNumber = slea.readInt();
+		int serverId = slea.readInt(); // server Id
 		String username = slea.readNullTerminatedAsciiString();
+		int loginServerType = slea.readInt(); // login server type
+		int clientSeed = slea.readInt(); // client seed
+		int region = slea.readInt(); // region
+		int battleGroup = slea.readInt(); // battle group
+		int realmIndex = slea.readInt(); // realm index
+		long unk4 = slea.readLong(); // unk4
+		byte[] digest = slea.read(20);
 		System.out.println("Build Number: " + buildNumber);
+		System.out.println("Server ID: " + serverId);
 		System.out.println("Username: " + username);
+		System.out.println("Login Server Type: " + loginServerType);
+		System.out.println("Client Seed: " + clientSeed);
+		System.out.println("Region: " + region);
+		System.out.println("Battle Group: " + battleGroup);
+		System.out.println("Realm Index: " + realmIndex);
+		System.out.println("Unk4: " + unk4);
+		System.out.println("Digest: " + Arrays.toString(digest));
 		LOGGER.info(slea.toString());
 	}
 }
